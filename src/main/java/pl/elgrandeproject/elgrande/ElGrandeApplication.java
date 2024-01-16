9363195074e3f3d33d2bcbfc3e9072133bd46f6d
nikolawierzbowska <nikolawierzbowska21@gmail.com>
2024-01-16 09:59:54 +0100

    configuration of project

---
+++ src/main/java/pl/elgrandeproject/elgrande/ElGrandeApplication.java
@@ -0,0 +1,13 @@
+package pl.elgrandeproject.elgrande;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElGrandeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElGrandeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/elgrandeproject/elgrande/ElGrandeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.elgrandeproject.elgrande;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElGrandeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
