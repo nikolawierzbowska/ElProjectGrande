59b2b71d1db226d909e124276c5790a83fa16da4
nikolawierzbowska <nikolawierzbowska21@gmail.com>
2024-02-17 21:16:55 +0100

    Update db

--- src/main/resources/db/migration/postgres/V1_0__schema.sql
+++ src/main/resources/db/migration/postgres/V1_0__schema.sql
@@ -1,38 +1,39 @@
 create table users
 (
-    id                uuid not null primary key,
-    email             varchar(30) unique,
+    id                UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    email             varchar(30) not null unique,
     first_name        varchar(15) not null,
     last_name         varchar(20) not null,
-    password          varchar(15) not null,
-    repeated_password varchar(15) not null
+    password          varchar(100) not null,
+    repeated_password varchar(100) not null
 );
 
 create table roles
 (
-    id   uuid not null primary key,
-    name varchar(10)
+    id  UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    name varchar(10) NOT NULL UNIQUE
 );
 
 create table courses
 (
-    id   uuid not null primary key,
-    name varchar(50) not null
+    id   UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    name varchar(50) not null UNIQUE
 );
 
 create table opinions
-(
-    courses_id  uuid  references courses,
-    id          uuid not null primary key,
-    users_id    uuid not null references users,
+(   courseS_id uuid,
+    FOREIGN KEY (courses_id) REFERENCES  courses (id),
+    id          UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     description varchar(200) not null,
-    user_name  varchar(50) not null
+    user_name  varchar(15) not null
 );
 
 create table users_roles
 (
-    role_id uuid not null references roles,
-    user_id uuid not null references users,
-    primary key (role_id, user_id)
+    user_id UUID,
+    role_id UUID,
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (role_id) REFERENCES roles (id),
+    UNIQUE (user_id, role_id)
 );
 
